931103443f448d7ee4f14b62349930b3215c1aab
Zasmaa <39232461+Zasmaa@users.noreply.github.com>
2018-08-18 00:08:44 -0500

    Update main.js

--- mws-restaurant-stage-1/js/main.js
+++ mws-restaurant-stage-1/js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: 'sk.eyJ1IjoiemFzbWFhIiwiYSI6ImNqa3l3bGdrMzBud3czcGxiM2I0b21rYXMifQ.uJroXLLbGyrPtf-zxaEQZw',
+    mapboxToken: '',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
